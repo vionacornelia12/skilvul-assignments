f30f8fac63ce61ddb3fabe3dcd20ea5aa8802f36
Viona Cornelia <vionacornelia12@gmail.com>
2023-05-23 10:14:04 +0700

    change layout style

--- ToDoListApp/src/components/Layout.jsx
+++ ToDoListApp/src/components/Layout.jsx
@@ -1,51 +1,51 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
 import { actions } from '../features/todos/todosSlice';
 import { useDispatch, useSelector } from "react-redux";
 
 const Layout = () => {
-    const userInput = useSelector(state => state.todos.userInput);
-    const dispatch = useDispatch();
+  const userInput = useSelector(state => state.todos.userInput);
+  const dispatch = useDispatch();
 
-    const handleCreateTodo = (e) => {
-        e.preventDefault();
-        dispatch(actions.createTodo())
-    }
+  const handleCreateTodo = (e) => {
+    e.preventDefault();
+    dispatch(actions.createTodo());
+  }
 
-    const handleSetUserInput = (userInput) => {
-        dispatch(actions.setUserInput({ userInput }))
-    }
+  const handleSetUserInput = (userInput) => {
+    dispatch(actions.setUserInput({ userInput }));
+  }
 
-    return (
-        <div>
-            <h1 className="flex gap-2 p-2" class="text-center font-bold text-2xl">What's the plan for today?</h1>
-            <form className="flex gap-2 p-2" onSubmit={handleCreateTodo}>
-                <input 
-                    type="text"
-                    value={userInput}
-                    onChange={(e) => handleSetUserInput(e.target.value)}
-                    placeholder="What to do"
-                    className="p-2 w-full border-grey border-solid border-2 rounded"
-                />
-                <input type="submit" className="btn w-20 bg-[#6558F5] text-white font-medium" value="Add" />
-            </form>
-            <nav >
-                <ul className="flex gap-2 p-2">
-                    <li >
-                        <Link to={"/"}>ALL</Link>
-                    </li>
-                    <li>
-                        <Link to={"/active"}>ACTIVE</Link>
-                    </li>
-                    <li>
-                        <Link to={"/completed"}>COMPLETED</Link>
-                    </li>
-                </ul>
-            </nav>
-            <section className="p-2">
-                <Outlet />
-            </section>
-        </div>
-    )
+  return (
+    <div className="container mx-auto mt-10 px-4">
+      <h1 className="text-center font-bold text-2xl">What's the plan for today?</h1>
+      <form className="flex gap-2 mt-4" onSubmit={handleCreateTodo}>
+        <input 
+          type="text"
+          value={userInput}
+          onChange={(e) => handleSetUserInput(e.target.value)}
+          placeholder="What to do"
+          className="p-2 w-full border-gray-300 border-solid border-2 rounded"
+        />
+        <input type="submit" className="btn bg-[#6558F5] text-white font-medium" value="Add" />
+      </form>
+      <nav className="mt-4">
+        <ul className="flex gap-2">
+          <li>
+            <Link to={"/"}>ALL</Link>
+          </li>
+          <li>
+            <Link to={"/active"}>ACTIVE</Link>
+          </li>
+          <li>
+            <Link to={"/completed"}>COMPLETED</Link>
+          </li>
+        </ul>
+      </nav>
+      <section className="mt-4">
+        <Outlet />
+      </section>
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
